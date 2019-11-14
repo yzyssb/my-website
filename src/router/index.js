8f43ce56b4fce7ee899ba0c3eb6db6623d798dc0
yangzhiyuan <yangzhiyuan@dazuhang.com>
2019-11-14 18:56:36 +0800

    增加路由处理，但依旧还有问题
    Signed-off-by: yangzhiyuan <yangzhiyuan@dazuhang.com>

--- src/router/index.js
+++ src/router/index.js
@@ -3,29 +3,37 @@
 var router = new VueRouter({
   mode: 'history',
   base: '/my-favor/',
-  routes: [{ //首页
-      path: '/',
-      name: 'index',
-      component: resolve => require(['../pages/index'], resolve),
-      meta: {
-        keepAlive: true
-      }
-    },
-    { //文章详情
-      path: '/article',
-      name: 'articleDetail',
-      component: resolve => require(['../pages/articleDetail'], resolve)
-    },
-    { //测试页：上传图片
-      path: '/test',
-      name: 'test',
-      component: resolve => require(['../pages/test'], resolve)
-    },
-    { //测试页
-      path: '/test1',
-      name: 'test1',
-      component: resolve => require(['../pages/test1'], resolve)
-    },
+  routes: [{ //*
+    path: '*',
+    redirect: '/',
+    component: resolve => require(['../pages/index'], resolve),
+    meta: {
+      keepAlive: true
+    }
+  },
+  { //首页
+    path: '/',
+    name: 'index',
+    component: resolve => require(['../pages/index'], resolve),
+    meta: {
+      keepAlive: true
+    }
+  },
+  { //文章详情
+    path: '/article',
+    name: 'articleDetail',
+    component: resolve => require(['../pages/articleDetail'], resolve)
+  },
+  { //测试页：上传图片
+    path: '/test',
+    name: 'test',
+    component: resolve => require(['../pages/test'], resolve)
+  },
+  { //测试页
+    path: '/test1',
+    name: 'test1',
+    component: resolve => require(['../pages/test1'], resolve)
+  },
   ]
 })
 
